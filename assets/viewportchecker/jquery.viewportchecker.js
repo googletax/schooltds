2e0cabb9b2550af3276c969606b8c933f332d25c
googletax <35454545+googletax@users.noreply.github.com>
2019-10-20 15:07:16 +0530

    create github pages

--- assets/viewportchecker/jquery.viewportchecker.js
+++ assets/viewportchecker/jquery.viewportchecker.js
@@ -1,21 +1,38 @@
-/*
-    The MIT License (MIT)
+/**
+ * jQuery-viewport-checker - v1.8.8 - 2017-09-25
+ * https://github.com/dirkgroenen/jQuery-viewport-checker
+ *
+ * Copyright (c) 2017 Dirk Groenen
+ * Licensed MIT <https://github.com/dirkgroenen/jQuery-viewport-checker/blob/master/LICENSE>
+ */
 
-    Copyright (c) 2014 Dirk Groenen
+!function(a){a.fn.viewportChecker=function(b){var c={classToAdd:"visible",classToRemove:"invisible",classToAddForFullView:"full-visible",removeClassAfterAnimation:!1,offset:100,repeat:!1,invertBottomOffset:!0,callbackFunction:function(a,b){},scrollHorizontal:!1,scrollBox:window};a.extend(c,b);var d=this,e={height:a(c.scrollBox).height(),width:a(c.scrollBox).width()};return this.checkElements=function(){var b,f;c.scrollHorizontal?(b=Math.max(a("html").scrollLeft(),a("body").scrollLeft(),a(window).scrollLeft()),f=b+e.width):(b=Math.max(a("html").scrollTop(),a("body").scrollTop(),a(window).scrollTop()),f=b+e.height),d.each(function(){var d=a(this),g={},h={};if(d.data("vp-add-class")&&(h.classToAdd=d.data("vp-add-class")),d.data("vp-remove-class")&&(h.classToRemove=d.data("vp-remove-class")),d.data("vp-add-class-full-view")&&(h.classToAddForFullView=d.data("vp-add-class-full-view")),d.data("vp-keep-add-class")&&(h.removeClassAfterAnimation=d.data("vp-remove-after-animation")),d.data("vp-offset")&&(h.offset=d.data("vp-offset")),d.data("vp-repeat")&&(h.repeat=d.data("vp-repeat")),d.data("vp-scrollHorizontal")&&(h.scrollHorizontal=d.data("vp-scrollHorizontal")),d.data("vp-invertBottomOffset")&&(h.scrollHorizontal=d.data("vp-invertBottomOffset")),a.extend(g,c),a.extend(g,h),!d.data("vp-animated")||g.repeat){String(g.offset).indexOf("%")>0&&(g.offset=parseInt(g.offset)/100*e.height);var i=g.scrollHorizontal?d.offset().left:d.offset().top,j=g.scrollHorizontal?i+d.width():i+d.height(),k=Math.round(i)+g.offset,l=g.scrollHorizontal?k+d.width():k+d.height();g.invertBottomOffset&&(l-=2*g.offset),k<f&&l>b?(d.removeClass(g.classToRemove),d.addClass(g.classToAdd),g.callbackFunction(d,"add"),j<=f&&i>=b?d.addClass(g.classToAddForFullView):d.removeClass(g.classToAddForFullView),d.data("vp-animated",!0),g.removeClassAfterAnimation&&d.one("webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend",function(){d.removeClass(g.classToAdd)})):d.hasClass(g.classToAdd)&&g.repeat&&(d.removeClass(g.classToAdd+" "+g.classToAddForFullView),g.callbackFunction(d,"remove"),d.data("vp-animated",!1))}})},("ontouchstart"in window||"onmsgesturechange"in window)&&a(document).bind("touchmove MSPointerMove pointermove",this.checkElements),a(c.scrollBox).bind("load scroll",this.checkElements),a(window).resize(function(b){e={height:a(c.scrollBox).height(),width:a(c.scrollBox).width()},d.checkElements()}),this.checkElements(),this}}(jQuery);
 
-    Permission is hereby granted, free of charge, to any person obtaining a copy of
-    this software and associated documentation files (the "Software"), to deal in
-    the Software without restriction, including without limitation the rights to
-    use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
-    the Software, and to permit persons to whom the Software is furnished to do so,
-    subject to the following conditions:
+// Mobirise initialization
+var isBuilder = $('html').hasClass('is-builder');
+if(!isBuilder){
+    $('.counters').each(function(){
+        $(this).viewportChecker({
+            offset:200,
+            callbackFunction: function(elem,action){
+                $('#' + elem.attr('id') + ' .count').each(function() {
+                    var value = $(this).text().trim();
+                    var counter = (value.length - (value.indexOf(".") + 1)) > 2 ? value.replace(/\./ig, "") : Math.floor(value);
 
-    The above copyright notice and this permission notice shall be included in all
-    copies or substantial portions of the Software.
-*/
-!function(c){c.fn.viewportChecker=function(g){var e={classToAdd:"visible",classToRemove:"invisible",classToAddForFullView:"full-visible",removeClassAfterAnimation:!1,offset:100,repeat:!1,invertBottomOffset:!0,callbackFunction:function(c,e){},scrollHorizontal:!1,scrollBox:window};c.extend(e,g);var n=this,k=c(e.scrollBox).height(),p=c(e.scrollBox).width();return this.checkElements=function(){var f,h;e.scrollHorizontal?(f=Math.max(c("html").scrollLeft(),c("body").scrollLeft(),c(window).scrollLeft()),
-h=f+p):(f=Math.max(c("html").scrollTop(),c("body").scrollTop(),c(window).scrollTop()),h=f+k);n.each(function(){var a=c(this),b={},d={};if(a.data("vp-add-class")&&(d.classToAdd=a.data("vp-add-class")),a.data("vp-remove-class")&&(d.classToRemove=a.data("vp-remove-class")),a.data("vp-add-class-full-view")&&(d.classToAddForFullView=a.data("vp-add-class-full-view")),a.data("vp-keep-add-class")&&(d.removeClassAfterAnimation=a.data("vp-remove-after-animation")),a.data("vp-offset")&&(d.offset=a.data("vp-offset")),
-a.data("vp-repeat")&&(d.repeat=a.data("vp-repeat")),a.data("vp-scrollHorizontal")&&(d.scrollHorizontal=a.data("vp-scrollHorizontal")),a.data("vp-invertBottomOffset")&&(d.scrollHorizontal=a.data("vp-invertBottomOffset")),c.extend(b,e),c.extend(b,d),!a.data("vp-animated")||b.repeat){0<String(b.offset).indexOf("%")&&(b.offset=parseInt(b.offset)/100*k);var d=b.scrollHorizontal?a.offset().left:a.offset().top,g=b.scrollHorizontal?d+a.width():d+a.height(),l=Math.round(d)+b.offset,m=b.scrollHorizontal?l+
-a.width():l+a.height();b.invertBottomOffset&&(m-=2*b.offset);l<h&&m>f?(a.removeClass(b.classToRemove),a.addClass(b.classToAdd),b.callbackFunction(a,"add"),g<=h&&d>=f?a.addClass(b.classToAddForFullView):a.removeClass(b.classToAddForFullView),a.data("vp-animated",!0),b.removeClassAfterAnimation&&a.one("webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend",function(){a.removeClass(b.classToAdd)})):a.hasClass(b.classToAdd)&&b.repeat&&(a.removeClass(b.classToAdd+" "+b.classToAddForFullView),
-b.callbackFunction(a,"remove"),a.data("vp-animated",!1))}})},("ontouchstart"in window||"onmsgesturechange"in window)&&c(document).bind("touchmove MSPointerMove pointermove",this.checkElements),c(e.scrollBox).bind("load scroll",this.checkElements),c(window).resize(function(f){k=c(e.scrollBox).height();p=c(e.scrollBox).width();n.checkElements()}),this.checkElements(),this}}(jQuery);var isBuilder=$("html").hasClass("is-builder");
-isBuilder||$(".counters").each(function(){$(this).viewportChecker({callbackFunction:function(c,g){$("#"+c.attr("id")+" .count").each(function(){$(this).prop("Counter",0).animate({Counter:$(this).text()},{duration:3E3,easing:"swing",step:function(c){$(this).text(Math.ceil(c))}})})}})});
+                    $(this).prop('Counter', 0).animate({
+                        Counter: counter
+                    }, {
+                        duration: 3000,
+                        easing: 'swing',
+                        step: function(now) {
+                            $(this).text(Math.ceil(now));
+                        },
+                        done: function () {
+                            $(this).text(value);
+                        }
+                    });
+                });
+            }
+        });
+    });
+}
